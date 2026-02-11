3e43636bf206c4cbb4fd89761723adffdd1d52aa
ZeronFX <axiomaticretroactivity@gmail.com>
2026-02-10 18:48:41 -0600

    add thumbnail uploading to memory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +30,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to parse file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	thumbnailImage, thumbnailHeaderPtr, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to form file", err)
+		return
+	}
+	thumbnailMediaType := thumbnailHeaderPtr.Header.Get("Content-Type")
+
+	thumbnailData, err := io.ReadAll(thumbnailImage)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not read file into bytes", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusForbidden, "Video does not belong to current user", err)
+		return
+	}
+
+	thumbnail := thumbnail{
+		data:      thumbnailData,
+		mediaType: thumbnailMediaType,
+	}
+	videoThumbnails[videoID] = thumbnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	videoMetadata.ThumbnailURL = &thumbnailURL
+	videoMetadata.UpdatedAt = time.Now()
+
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
